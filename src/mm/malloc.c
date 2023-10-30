d00238fddae4e96e259ee13a16e8d3a09c6fda4d
cristian.tudor1607 <cristian.tudor1607@stud.acs.upb.ro>
2023-10-30 10:00:13 +0000

    Working memory management

--- src/mm/malloc.c
+++ src/mm/malloc.c
@@ -7,32 +7,86 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 void *malloc(size_t size)
 {
 	if  (size == 0)
 		return NULL;
 
+	// Map the required chunk of memory using mmap
+	void *chunk = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (chunk == MAP_FAILED)
+		return NULL;
+
+	// Add the new chunk in the list
+	// TODO: check if fail
+	int ret = mem_list_add(chunk, size);
+	
+	return chunk;
 }
 
 void *calloc(size_t nmemb, size_t size)
 {
-	/* TODO: Implement calloc(). */
-	return NULL;
+	// Get a new chunk of memory
+	void *mem_chunk = malloc(nmemb * size);
+	if (mem_chunk == NULL)
+		return NULL;
+
+	// Set all the bytes to 0
+	for (size_t i = 0; i < nmemb * size; i++)
+		*((char *) mem_chunk + i) = 0;
+	
+	return mem_chunk;
 }
 
 void free(void *ptr)
 {
-	/* TODO: Implement free(). */
+	// If ptr is NULL, do nothing
+	if (!ptr)
+		return;
+
+	// Find the chunk of memory that is about to be deleted
+	struct mem_list *chunk = mem_list_find(ptr);
+	if (!chunk)
+		return;
+
+	size_t size = chunk->len;
+	
+	// munmap shouldn't fail, because the ptr was already verified;
+	// If the chunk pointed by ptr failed mapping, or it wasn't allocated by
+	// malloc, it wouldn't have been added in the list and found by
+	// mem_list_find function
+	munmap(ptr, size);
+	mem_list_del(ptr);
+	
+	// Don't forget to set the ptr to NULL :)
+	ptr = NULL;
 }
 
 void *realloc(void *ptr, size_t size)
-{
-	/* TODO: Implement realloc(). */
-	return NULL;
+{	
+	// If ptr is NULL, then is the same as malloc(size)
+	if (!ptr)
+		return malloc(size);
+
+	// If the size is 0, then is the same as free(ptr)
+	if (!size) {
+		free(ptr);
+		return NULL;
+	}
+
+	// Get the old size of the chunk
+	struct mem_list *chunk = mem_list_find(ptr);
+	if (!chunk)
+		return NULL;
+
+	size_t old_size = chunk->len;
+	return mremap(ptr, old_size, size, MREMAP_MAYMOVE);
 }
 
 void *reallocarray(void *ptr, size_t nmemb, size_t size)
 {
-	/* TODO: Implement reallocarray(). */
-	return NULL;
+	// As the manual page says, the reallocarray call is equivalent to
+	// realloc(ptr, nmemb * size)
+	return realloc(ptr, nmemb * size);
 }
--- src/mm/mmap.c
+++ src/mm/mmap.c
@@ -3,35 +3,28 @@
 #include <sys/mman.h>
 #include <errno.h>
 #include <internal/syscall.h>
-#include <sys/stat.h>
 
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
-	// check the file descriptor
-	struct stat file_status;
-	if (fstat(fd, &file_status) == -1 && fd != -1) {
-		errno = EBADF;
+	// Because syscall returns a long value, it will later be casted to void *
+	long chunk = syscall(__NR_mmap, addr, length, prot, flags, fd, offset);
+	if (chunk < 0) {
+		errno = -chunk;
 		return MAP_FAILED;
 	}
 
-	void *area = (void *)syscall(__NR_mmap, addr, length, prot, flags, fd, offset);
-	if (area == MAP_FAILED) {
-		errno = - (long)area;
-		return MAP_FAILED;
-	}
-
-	return area;
+	return (void *) chunk;
 }
 
 void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 {
-	void *area = (void *)syscall(__NR_mremap, old_address, old_size, new_size, flags);
-	if (area == MAP_FAILED) {
-		errno = - (long)area;
+	void *rechunk = (void *)syscall(__NR_mremap, old_address, old_size, new_size, flags);
+	if (rechunk == MAP_FAILED) {
+		errno = - (long)rechunk;
 		return MAP_FAILED;
 	}
 
-	return area;
+	return rechunk;
 }
 
 int munmap(void *addr, size_t length)
